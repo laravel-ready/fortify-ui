b6bfe291d52194b36030fbaa94e70425b615d4ba
Egoist <epsilonelittr@gmail.com>
2022-11-18 00:34:30 +0300

    Add icon components

---
+++ resources/views/starter/components/social-register.blade.php
@@ -0,0 +1,11 @@
+<!-- Splitter -->
+<div class="splitter-container">
+    <div class="splitter">
+        <!-- Splitter Background -->
+        <div class="splitter-background">
+            <span>
+                {{ __('fortify-ui::auth.splitter_or_register_with') }}
+            </span>
+        </div>
+    </div>
+</div>
