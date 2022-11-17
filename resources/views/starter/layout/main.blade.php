acfa0cac8a612f4a29bef93390c5568a46378b1c
Egoist <epsilonelittr@gmail.com>
2022-11-18 01:48:23 +0300

    Add auth routes

--- resources/views/starter/layout/main.blade.php
+++ resources/views/starter/layout/main.blade.php
@@ -106,14 +106,14 @@
                     <!-- Link List -->
                     <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:border-gray-700">
                         <li>
-                            <a href="#"
+                            <a href="{{ route('login') }}"
                                 class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                                 Login
                             </a>
                         </li>
 
                         <li>
-                            <a href="#"
+                            <a href="{{ route('register') }}"
                                 class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                                 Register
                             </a>
