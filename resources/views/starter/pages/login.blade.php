ecb264e9b7f2acd8361bc5c60816542a6e127b65
Egoist <epsilonelittr@gmail.com>
2023-03-01 06:13:39 +0300

    Add shared script

--- resources/views/starter/pages/login.blade.php
+++ resources/views/starter/pages/login.blade.php
@@ -101,35 +101,4 @@
     </form>
 @endsection
 
-@section('footerScripts')
-    <script>
-        window.addEventListener('load', function() {
-            // #region Password Eye Toggler
-
-            const eyeToggleButton = document.getElementById('eye-toggle');
-
-            if (eyeToggleButton) {
-                const passwordInput = document.getElementById('password'),
-                    eyeOpen = document.getElementById('eye-open'),
-                    eyeClose = document.getElementById('eye-close');
-
-                eyeToggleButton.addEventListener('click', function() {
-                    console.log(passwordInput);
-
-                    if (passwordInput.type === 'password') {
-                        passwordInput.type = 'text';
-                        eyeOpen.style.display = 'block';
-                        eyeClose.style.display = 'none';
-                    } else {
-                        passwordInput.type = 'password';
-                        eyeOpen.style.display = 'none';
-                        eyeClose.style.display = 'block';
-
-                    }
-                });
-            }
-
-            // #endregion
-        });
-    </script>
-@endsection()
+@include('fortify-ui::starter.components.partials.eye-toggle-script')
--- resources/views/starter/pages/register.blade.php
+++ resources/views/starter/pages/register.blade.php
@@ -113,37 +113,4 @@
     </form>
 @endsection
 
-@section('footerScripts')
-    <script>
-        window.addEventListener('load', function() {
-            // #region Password Eye Toggler
-
-            const eyeToggleButtons = document.querySelectorAll('#eye-toggle');
-
-            if (eyeToggleButtons && eyeToggleButtons.length > 0) {
-                eyeToggleButtons.forEach(eyeToggleButton => {
-                    const dataInputId = eyeToggleButton.getAttribute('data-input-id');
-
-                    if (dataInputId) {
-                        cosnt passwordInput = document.querySelector(`input[id="${dataInputId}"]`),
-                            eyeOpen = document.getElementById('eye-open'),
-                            eyeClose = document.getElementById('eye-close');
-
-                        eyeToggleButton.addEventListener('click', function() {
-                            passwordInput.type = passwordInput.type === 'password' ? 'text' :
-                                'password';
-                            eyeOpen.style.display = passwordInput.type === 'password' ? 'block' :
-                                'none';
-                            eyeClose.style.display = passwordInput.type === 'password' ? 'none' :
-                                'block';
-                        });
-                    } else {
-                        console.error('Eye Toggle Button must have a data-input-id attribute.');
-                    }
-                });
-            }
-
-            // #endregion
-        });
-    </script>
-@endsection()
+@include('fortify-ui::starter.components.partials.eye-toggle-script')
