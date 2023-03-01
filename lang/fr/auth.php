6616c07da769edde82dff09d70f8b35d26976b5c
Egoist <epsilonelittr@gmail.com>
2023-03-01 06:44:31 +0300

    Add français translations

---
+++ lang/fr/auth.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'login' => 'Connexion',
+    'login_button' => 'Se connecter',
+    'register' => 'Inscription',
+    'register_button' => 'S\'inscrire',
+    'logout' => 'Déconnexion',
+    'confirm_password' => 'Confirmer le mot de passe',
+    'confirm_password_button' => 'Confirmer',
+    'email_verification' => 'Vérification de l\'adresse e-mail',
+    'form' => [
+        'remember_me' => 'Se souvenir de moi',
+        'placeholder_email_address' => 'Adresse e-mail',
+        'placeholder_password' => 'Mot de passe',
+        'placeholder_password_confirmation' => 'Confirmez le mot de passe',
+        'link_forgot_your_password' => 'Mot de passe oublié ?',
+        'link_do_you_have_an_account' => 'Vous avez un compte ?',
+        'splitter_or_login_with' => 'Ou connectez-vous avec votre e-mail / nom d\'utilisateur',
+        'splitter_or_register_with' => 'Ou inscrivez-vous avec votre e-mail / nom d\'utilisateur',
+        'placeholder_name' => 'Nom / Prénom',
+        'placeholder_username' => 'Nom d\'utilisateur',
+    ],
+    'verification' => [
+        'notice' => [
+            'welcome' => 'Bienvenue',
+            'resend_verification_email' => 'Renvoyer l\'e-mail de vérification',
+            'email_verification_sent' => 'Il vous reste une étape pour finaliser votre inscription. Veuillez vérifier votre e-mail et cliquer sur le lien de vérification. Si vous ne le trouvez pas, vérifiez votre dossier spam.',
+            'if_you_did_not_receive_the_email' => 'Si vous n\'avez pas reçu l\'e-mail, veuillez :link',
+            'if_you_did_not_receive_the_email_click_here' => 'nous contacter.',
+        ]
+    ],
+];
